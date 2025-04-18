6521d419a6d510a2a32b19262ed6f3ba45397707
Arsalaan Ahmad <arsalaan745@gmail.com>
2025-04-18 15:58:53 +0530

    Zod common schemas added

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,31 +1,39 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import {z} from "zod";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import { CreateUserSchema } from "@repo/common/types";
 
 const app = express();
 
-app.post("/signup", (req, res)=>{
+app.post("/signup", (req, res) => {
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
+   res.json({
+      message: "Incorrect inputs",
+    });
+    return;
+  }
 
-})
+  res.json({
+    userId: "123",
+  });
+});
 
-app.post("/signin", (req, res)=>{
-    
+app.post("/signin", (req, res) => {
+  const userId = 1;
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
+  res.json({
+    token,
+  });
+});
 
-    const userId = 1;
-    const token = jwt.sign({
-        userId
-    }, JWT_SECRET);
-    res.json({
-        token
-    })
-})
-
-app.post("/room", middleware, (req, res)=>{
-    
-})
-
+app.post("/room", middleware, (req, res) => {});
 
 app.listen(3001, () => {
   console.log("Server running on port 3001");
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,27 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", function connection(ws) {
+wss.on("connection", function connection(ws, request) {
+    const url = request.url;
+    if(!url){
+        return;
+    }
+    const queryParams = new URLSearchParams(url.split("?")[1]);
+    const token = queryParams.get("token") || ""; // || "" used this fallback to get rid of type error
     
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if(typeof decoded == "string"){
+        ws.close();
+        return;
+    }
+    
+    if(!decoded || !decoded.userId){
+        ws.close();
+        return;
+    }
   ws.on("message", function message(data) {
     ws.send("pong");
   });
