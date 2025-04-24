02ec57a5ee9afc531588b5422ceeb56cac76b6fb
Arsalaan Ahmad <arsalaan745@gmail.com>
2025-04-24 12:45:39 +0530

    excelidraw landing page done

--- apps/excelidraw-frontend/components/AuthPage.tsx
+++ apps/excelidraw-frontend/components/AuthPage.tsx
@@ -1,10 +1,17 @@
+"use client";
 export function AuthPage({ isSignin }: { isSignin: boolean }) {
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
-      <div className=" p-2 m-2 bg-white rounded">
-        <input type="text" placeholder="Email"></input>
-        <input type="password" placeholder="password"></input>
-        <button onClick={() => {}}>{isSignin ? "Sign in" : "Sign up"}</button>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="p-6 m-2 bg-white rounded">
+        <div className="p-2">
+          <input type="text" placeholder="Email"></input>
+        </div>
+        <div className="p-2">
+          <input type="password" placeholder="Password"></input>
+        </div>
+        <div className="pt-2">
+          <button onClick={() => {}}>{isSignin ? "Signin" : "Signup"}</button>
+        </div>
       </div>
     </div>
   );
