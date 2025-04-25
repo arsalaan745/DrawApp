ebd57a200fbfdf3a7bdbf32932c318d9f42bcdf6
Arsalaan Ahmad <arsalaan745@gmail.com>
2025-04-25 21:34:51 +0530

    Added top bar with icons

--- apps/excelidraw-frontend/components/Canvas.tsx
+++ apps/excelidraw-frontend/components/Canvas.tsx
@@ -1,5 +1,9 @@
 import { initDraw } from "@/draw";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { IconButton } from "./IconButton";
+import { Circle, Pencil, RectangleHorizontalIcon } from "lucide-react";
+
+export type Tool = "circle" | "pencil" | "rect";
 
 export function Canvas({
   roomId,
@@ -9,6 +13,7 @@ export function Canvas({
   roomId: string;
 }) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [selectedTool, setSelectedTool] = useState<Tool>("circle");
 
   useEffect(() => {
     if (canvasRef.current) {
@@ -16,7 +21,47 @@ export function Canvas({
     }
   }, [canvasRef]);
 
-  return <div>
-      <canvas ref={canvasRef} width={2000} height={1000}></canvas>
+  return (<div style={{
+        height: "100vh",
+        overflow: "hidden",
+      }}>
+      <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight} ></canvas>
+      <TopBar selectedTool={selectedTool} setSelectedTool={setSelectedTool}/>
     </div>
+  );
+}
+
+function TopBar({selectedTool,setSelectedTool,}: 
+  { selectedTool: Tool;
+    setSelectedTool: (s: Tool) => void;
+}) {
+  return ( <div style={{
+        position: "fixed",
+        top: 10,
+        left: 10,
+      }}>
+      <div className="flex gap-2">
+        <IconButton
+          onClick={() => {
+            setSelectedTool("pencil");
+          }}
+          activated={selectedTool === "pencil"}
+          icon={<Pencil />}/>
+
+        <IconButton
+          onClick={() => {
+            setSelectedTool("rect");
+          }}
+          activated={selectedTool === "rect"}
+          icon={<RectangleHorizontalIcon />} ></IconButton>
+
+        <IconButton
+          onClick={() => {
+            setSelectedTool("circle");
+          }}
+          activated={selectedTool === "circle"}
+          icon={<Circle />} ></IconButton>
+      </div>
+    </div>
+  );
 }
